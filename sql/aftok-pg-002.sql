6c54ea59419150e87ab48a5d24f15cf0059aecfb
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2016-10-13 20:38:38 -0600

    Use dbmigrations to manage database state.

--- sql/aftok-pg-002.sql
+++
@@ -1,9 +0,0 @@
-alter table projects add depreciation_fn json not null default '{"type": "LinearDepreciation", "arguments": {"undep": 6, "dep": 60}}';
-
-alter table project_companions add invited_by uuid references users(id);
-update project_companions set invited_by = user_id; --
-alter table project_companions alter column invited_by set not null;
-
-alter table project_companions 
-add joined_at timestamp with time zone not null 
-default (now() at time zone 'UTC');
